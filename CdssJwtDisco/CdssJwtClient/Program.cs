b4ec72bd3b99a3541e1b360a483139bf00cff3a0
Arthur Gorr <art.gorr@gmail.com>
2020-02-24 22:10:30 -0600

    initial code for disco

---
+++ CdssJwtDisco/CdssJwtClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CdssJwtClient
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            var message = await client.GetStringAsync("https://localhost:44396/weatherforecast");
+
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+    }
+}
